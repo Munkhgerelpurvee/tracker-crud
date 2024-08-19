93956d406cc17345b1b207cef01d936c140fdc1c
Munkhgerelpurvee <164706050+Munkhgerelpurvee@users.noreply.github.com>
2024-08-19 12:43:42 +0800

    frontEnd-c data-gaa shud zam deer ugch data-gaa barij avahdaa   const { id } = req.params;   const { name, title } = req.body; gej avdag bolov.

--- tracker-service/main.js
+++ tracker-service/main.js
@@ -1,22 +1,20 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-const fs = require('fs');
+const fs = require("fs");
 
-// 
+//
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-// 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+//
 
 //
 
 app.get("/article", (req, res) => {
-
   res.json([
     { id: 1, title: "CRUD", name: "C" },
     { id: 2, title: "Create", name: "R" },
@@ -25,47 +23,60 @@ app.get("/article", (req, res) => {
   ]);
 });
 
-
-
-
 // Read from file system
 const content = fs.readFileSync("accounts.json", "utf-8");
 let accounts = JSON.parse(content);
 
-// CRUD:Read 
-app.get("/accounts/list", (req, res) => {
+/*
+* HTTP request methods                    path name: 
+* CREATE: POST                            / categories
+* READ: GET                               /categories /      ONE Category - /categories:id
+* UPDATE: PUT/ PATCH                      /categories:id
+* DELETE: DELETE                          /categories:id
+
+*/
+
+// CRUD:Read
+app.get("/accounts", (req, res) => {
   res.json(accounts);
 });
 
-
+// CRUD:Read ONE category-g
+app.get("/accounts/:id", (req, res) => {
+  const { id } = req.params;
+  const account = accounts.find(acc.id === id);
+  res.json(account);
+});
 
 //
 //CRUD:Create
-app.post("/accounts/create", (req, res) => {
+app.post("/accounts", (req, res) => {
   //   console.log(req.params);
   //   console.log(req.query);
   //   accounts.push({ title: "new accounts", name: "saraa" });
   //   res.json("SUCCESS");
-  const { title } = req.body;
-  const { name } = req.body;
-  console.log("=== Where is req.body ===",req.body);
-  
+  const { name, title } = req.body;
+
+  console.log("=== Where is req.body ===", req.body);
+
   accounts.push({
     id: new Date().toISOString(),
     title: title,
     name: name,
   });
 
-
   // Write to file system
   fs.writeFileSync("accounts.json", JSON.stringify(accounts));
   res.json(accounts);
 });
-//
+
+// Хэрэглэгчээс буюу FrontEnd-c юм дамжуулахад 2 төрлөөр дамжуулж өгч байгаа. Нэг нь бол шууд зам дээр нь ID өгнө (catergory/1234). Ингэж зам дээр нь  ID өгсөнийг // const { id, name, title } = req.params; гэж барьж авна. Нөгөө арга нь (/catergory?id=1234) req.query буюу const { id, name, title } = req.query; гэж утгыг нь барьж авна.
+
 //CRUD:Update
-app.put("/accounts/update", (req, res) => {
-  //
-  const { id, name, title } = req.query;
+app.put("/accounts/:id", (req, res) => {
+  //parameter-ээр орж ирж байгаа зүйлийг  const { id, name, title } = req.query req.query -гэж барьж авдаг байгаа.
+  const { id } = req.params;
+  const { name, title } = req.body;
   // index-ийг нь id-гаар нь олж засах
   const index = accounts.findIndex((acc) => acc.id === id);
   accounts[index].name = name;
@@ -75,22 +86,22 @@ app.put("/accounts/update", (req, res) => {
   res.json("SUCCESS");
 });
 // http://localhost:4000/accounts/update?id=2024-08-17T10:22:46.286Z&name=Bold&title=harvest
+
 //
 //CRUD:Delete 200ok
-app.delete("/accounts/Delete", (req, res) => {
+app.delete("/accounts/:id", (req, res) => {
   //
-  const { id } = req.query;
+  const { id } = req.params;
   console.log({ id });
+
   accounts = accounts.filter((acc) => acc.id !== id);
   fs.writeFileSync("accounts.json", JSON.stringify(accounts));
   res.json("SUCCESS");
 });
 
-
-
 // CRUD: Create, Read, Update, Delete
 
 const port = 4000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- tracker-web/app/page.js
+++ tracker-web/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
   // Refresh хийхэд биш харин бичмэгч автоматаар орж ирдэг болгоё гэвэл нэг function дотор бичиж өгөөд харин дараа нь түүнийг useEffect дотор нэг удаа дуудаж авдаг болгоно.  Мөн function createNewAccount дотор дуудчих юм бол дахин шинэчлэгдэж орж ирэх байгаа.
 
   function loadAccount() {
-    fetch("http://localhost:4000/accounts/list")
+    fetch("http://localhost:4000/accounts")
       .then((res) => res.json())
       .then((data) => {
         console.log("Where is ACCOUNTS LIST Data", data);
@@ -33,19 +33,17 @@ export default function Home() {
   function createNewAccount() {
     const name = prompt("Name...");
     const title = prompt("Title:");
-    fetch(`http://localhost:4000/accounts/create`, 
-      {method: "POST",
-      // Body нь өөрөө заавал string авдаг байгаа. Тэгхээр JSON файлаа string рүү хөрвүүлж бичих хэрэгтэй байгаа. Мөн ямар ч том хэмжээтэй текст авч чадна. 
-       body: JSON.stringify({
-        name:name,
-        title:title,
-        body_html:`сайн байна уу. Та бүхэндээ энэ сайхан өдрийн мэндийг хүргэж байна. Энэ body дотор ямар ч урт тэмдэгт явуулж болох байгаа. POST хүсэлтээр ямар ч өргөнтэй ямар ч тэмдэгт явуулж болно.Энэ зам дээр л урт болон тэмдэгтийн хязгаарлалт байдаг байгаа. http://localhost:4000/accounts/update?id=2024-08-17T10:22:46.286Z&name=Bold&title=harvest`
-       },
+    fetch(`http://localhost:4000/accounts`, {
+      method: "POST",
+      // Body нь өөрөө заавал string авдаг байгаа. Тэгхээр JSON файлаа string рүү хөрвүүлж бичих хэрэгтэй байгаа. Мөн ямар ч том хэмжээтэй текст авч чадна.
+      body: JSON.stringify({
+        name: name,
+        title: title,
+        body_html: `сайн байна уу. Та бүхэндээ энэ сайхан өдрийн мэндийг хүргэж байна. Энэ body дотор ямар ч урт тэмдэгт явуулж болох байгаа. POST хүсэлтээр ямар ч өргөнтэй ямар ч тэмдэгт явуулж болно.Энэ зам дээр л урт болон тэмдэгтийн хязгаарлалт байдаг байгаа. http://localhost:4000/accounts/update?id=2024-08-17T10:22:46.286Z&name=Bold&title=harvest`,
+      }),
 
-         ),
-      
-       headers: {"Content-type":"application/json; charset=UTF-8"},
-       })
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
       .then((res) => res.json())
       .then(() => {
         loadAccount();
@@ -56,14 +54,14 @@ export default function Home() {
 
   //
   return (
-    <main className="flex min-h-screen flex-col p-2 gap-4">
+    <main className="flex flex-col min-h-screen gap-4 p-2">
       <>
         {records.map((record) => {
           return (
             <>
               <div key={record.id} className="flex">
                 {" "}
-                <p className="bg-gray-200 gap-4 rounded-full">
+                <p className="gap-4 bg-gray-200 rounded-full">
                   {`№:${record.id}  ${record.title}`}
                 </p>
                 <br />
@@ -78,21 +76,21 @@ export default function Home() {
         <div className="flex">
           <button
             onClick={createNewAccount}
-            className="bg-blue-800 rounded-full px-4 text-white"
+            className="px-4 text-white bg-blue-800 rounded-full"
           >
             Add New account
           </button>
         </div>
-        <div className=" pr-4">
+        <div className="pr-4 ">
           {accounts.map((item, index) => {
             return (
-              <div key={item.name + index} className="text-gray-600 gap-4 pr-4">
+              <div key={item.name + index} className="gap-4 pr-4 text-gray-600">
                 <div>
                   {`Name:${item.name}`} {`Tiitle:${item.title}`}
-                  <button className="bg-blue-800 rounded-full px-4 text-white">
+                  <button className="px-4 text-white bg-blue-800 rounded-full">
                     Edit
                   </button>
-                  <button className="bg-blue-800 rounded-full px-4 text-white">
+                  <button className="px-4 text-white bg-blue-800 rounded-full">
                     Delete
                   </button>
                 </div>
